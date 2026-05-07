8ad132f555feb7aef64824883638430db8879966
ursinho-dev <joseurcianofsf@gmail.com>
2026-05-07 10:46:28 -0300

    errors e mcors

---
+++ src/middlewares/errors.middleware.js
@@ -0,0 +1,18 @@
+export const notFound = (req, res, next) => {
+    res.status(404).json({
+      message: "Esta rota não existe na API!",
+      method: req.method,
+      path: req.originalUrl,
+      status: 404,
+    });
+  };
+  
+  export const errorHandler = (err, req, res, next) => {
+  
+    console.error(err.stack);
+  
+    return res.status(500).json({
+      message: "Erro interno do servidor",
+      error: err.message,
+    });
+  };
---
+++ src/middlewares/mcors.middleware.js
@@ -0,0 +1,8 @@
+import cors from "cors";
+
+export const mycors = cors({
+  origin: "http://localhost:5000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+});
