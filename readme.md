8ee6f8c9a63b205d26ee4536c72ce071bfc86a64
CesarCarlos7 <cesar.carlos10@aluno.ifce.edu.br>
2026-05-14 10:54:53 -0300

    inicio passo 2

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 Tema-->Barbearia: Agendamento de serviços e produtos para barba.
 Inspiraçao-->T.A Barbearia
+Equipe: Cesar Carlos mendes da Silva;Jose Urciano Ferreira Da Silva Filho
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "node:path";
 
 import apiRouter from "./routes/api.routes.js";
 
@@ -12,24 +13,25 @@ import {
 
 const app = express();
 
+
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "views"));
+
+
+app.use(express.static(path.join(import.meta.dirname, "../public")));
+
 app.use(mcors);
 app.use(express.json());
 app.use(debug);
 
 app.use("/api", apiRouter);
 
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+
 app.use(notFound);
 app.use(errorHandler);
 
 export default app;
- 
-import path from "node:path"; 
-
-
-app.set("view engine", "ejs"); 
-app.set("views", path.join(import.meta.dirname, "views"));
-app.use(express.static (path.join(import.meta.dirname, "public")));
- app.use(express.json()); 
- app.get("/home", (req, res) => { 
-}); 
-res.render("home");
